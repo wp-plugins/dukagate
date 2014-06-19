10369f4e2784c0ac31ecd940a38292478e359985
rixeo <rixeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 07:02:34 +0000

    Tagging version 3.7.2.2
    git-svn-id: https://plugins.svn.wordpress.org/dukagate/tags/3.7.2.2@934883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dukagate/plugins-gateway/bank_transfer.php
+++ dukagate/plugins-gateway/bank_transfer.php
@@ -1,11 +1,11 @@
 <?php
-	
+
 /**
  * DukaGate Payment Gateway
- * Cash On Delivery Gateway Plugin
+ * Bank Transfer Gateway Plugin
  */
-class DukaGate_GateWay_CashOnDelivery extends DukaGate_GateWay_API{
-
+class DukaGate_GateWay_BankTransfer extends DukaGate_GateWay_API{
+	
 	//private gateway slug. Lowercase alpha (a-z) and dashes (-) only please!
 	var $plugin_name;
 	
@@ -23,21 +23,20 @@ class DukaGate_GateWay_CashOnDelivery extends DukaGate_GateWay_API{
 	
 	//Determine if to use form submit or other method
 	var $form_submit = false;
-
-
+	
 	
 	//Default method called on create
 	function on_create(){
-		$this->plugin_name = __('Cash On Delivery');
-		$this->plugin_slug = __('cash_on_delivery');
-		$this->required_fields = array('none' =>'');
+		$this->plugin_name = __('Bank Transfer');
+		$this->plugin_slug = __('bank_transfer');
+		$this->required_fields = array('instructions' =>'');
 		$this->currencies = array();
 	}
 	
 	//Register Plugin
 	function register(){
 		//Register Plugin
-		dg_register_gateway_plugin('DukaGate_GateWay_CashOnDelivery', $this->plugin_name, $this->plugin_slug, $this->required_fields, $this->currencies);
+		dg_register_gateway_plugin('DukaGate_GateWay_BankTransfer', $this->plugin_name, $this->plugin_slug, $this->required_fields, $this->currencies);
 	}
 	
 	/**
@@ -104,14 +103,53 @@ class DukaGate_GateWay_CashOnDelivery extends DukaGate_GateWay_API{
 		
 		$output = $settings[$this->plugin_slug]['instructions'];
 		$output .= '<br/>';
-		$output .= '<button class="bank_continue_btn" onclick="cash_continue();">Continue</button>';
+		$output .= '<button class="bank_continue_btn" onclick="bank_continue();">Continue</button>';
 		$output .= '<script type="text/javascript">';
-        $output .= 'function cash_continue(){
+        $output .= 'function bank_continue(){
 					window.location.href="'.$return_path.'"
 					};
                      </script>';
 		return $output;
 	}
+	
+	function order_form_action($invoice){
+		global $dukagate;
+		if (! empty( $_POST ) && check_admin_referer('dg_bank_payments','dg_bank_payments_noncename') ){
+			$dg_bank_files = get_option('dg_bank_files');
+			if(!empty($_FILES['dg_bank_file']['name'])) {
+				// Get the file type of the upload
+				$arr_file_type = wp_check_filetype(basename($_FILES['dg_bank_file']['name']));
+				$uploaded_type = $arr_file_type['type'];
+				$upload = $dukagate->upload_file($_FILES['dg_bank_file']['name'], file_get_contents($_FILES['dg_bank_file']['tmp_name']), DG_DUKAGATE_INVOICE_DIR, DG_DUKAGATE_INVOICE_URL);
+				
+				$dg_bank_files[$invoice]['file'] = $upload['url'];
+				update_option('dg_bank_files', $dg_bank_files);
+			}
+			
+		}
+		$dg_bank_files = get_option('dg_bank_files');
+		?>
+		<tr>
+			<td><strong><?php _e("Upload a copy of successful EFT form", "dukagate"); ?></strong></td>
+			<td>
+				<?php
+					if((isset($dg_bank_files[$invoice]['file'])) && !empty($dg_bank_files[$invoice]['file'])){
+						echo sprintf( __('<a href="%1$s" target="_blank">EFT File</a>'), $dg_bank_files[$invoice]['file']);
+					}else{
+						?>
+						<form enctype="multipart/form-data" action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="post">
+							<?php wp_nonce_field('dg_bank_payments','dg_bank_payments_noncename'); ?>
+							<input class="button" type="file" id="dg_bank_file" name="dg_bank_file" value="" size="25" />
+							<p class="submit">
+								<input class='button-primary' type='submit' value='<?php _e('Save','dukagate'); ?>'/><br/>
+							</p>
+						</form>
+						<?php
+					}
+				?>
+			</td>
+		</tr>
+		<?php
+	}
 }
-
 ?>
