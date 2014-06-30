6169a47595fb1373d6d44ef5b702ea0441fecc6c
rixeo <rixeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-30 04:37:10 +0000

    Check payment mode
    git-svn-id: https://plugins.svn.wordpress.org/dukagate/trunk@940625 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dukagate/plugins-gateway/check.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * DukaGate Payment Gateway
+ * Check Gateway Plugin
+ */
+class DukaGate_GateWay_Check extends DukaGate_GateWay_API{
+	
+	//private gateway slug. Lowercase alpha (a-z) and dashes (-) only please!
+	var $plugin_name;
+	
+	//shortname of plugin
+	var $plugin_slug;
+	
+	//always contains the url to send payment notifications to if needed by your gateway. Populated by the parent class
+	var $ipn_url;
+	
+	//Payment gateway required fields
+	var $required_fields;
+	
+	//Currencies
+	var $currencies;
+	
+	//Determine if to use form submit or other method
+	var $form_submit = false;
+	
+	//Default method called on create
+	function on_create(){
+		$this->plugin_name = __('Check');
+		$this->plugin_slug = __('check');
+		$this->required_fields = array('instructions' =>'');
+		$this->currencies = array();
+	}
+	
+	//Register Plugin
+	function register(){
+		//Register Plugin
+		dg_register_gateway_plugin('DukaGate_GateWay_Check', $this->plugin_name, $this->plugin_slug, $this->required_fields, $this->currencies);
+	}
+	
+	/**
+	 * Process IPN
+	 */
+	function process_ipn_return() {
+		global $dukagate;
+		$dg_shop_settings = get_option('dukagate_shop_settings');
+		$invoice = $_REQUEST['invoice'];
+		$dukagate->dg_update_order_log($invoice, 'Pending');
+		
+		$return_path = get_page_link($dg_shop_settings['thankyou_page']);
+		$check_return_path = explode('?', $return_path);
+		if (count($check_return_path) > 1) {
+			$return_path .= '&id=' . $invoice;
+		} else {
+			$return_path .= '?id=' . $invoice;
+		}
+		header("Location: $return_path");
+	}
+	
+	/**
+	 * Set Up Payment gateway options
+	 */
+	function set_up_options($plugin_slug, $settings){
+		global $dukagate;
+		$editor_id = 'dg['.$plugin_slug.'][instructions]';
+		?>
+		<table class="form-table">
+			<tr>
+				<th scope="row"><?php _e('Instructions','dukagate'); ?></th>
+				<td>
+					<p>
+						<?php wp_editor( $settings[$plugin_slug]['instructions'], $editor_id , array( 'media_buttons' => false )); ?>
+					</p>
+				</td>
+			</tr>
+			
+		</table>
+		
+		<?php
+	}
+	
+	/**
+	 * Process Payment
+	 */
+	function process_payment_form($cart){
+		global $dukagate;
+		
+		$settings = get_option('dukagate_gateway_settings');
+		
+		$dg_shop_settings = get_option('dukagate_shop_settings');
+		$invoice = $_SESSION['dg_invoice'];
+		$dukagate->dg_update_order_log($invoice, 'Pending');
+		
+		$return_path = get_page_link($dg_shop_settings['thankyou_page']);
+		$check_return_path = explode('?', $return_path);
+		if (count($check_return_path) > 1) {
+			$return_path .= '&id=' . $invoice;
+		} else {
+			$return_path .= '?id=' . $invoice;
+		}
+		
+		$output = $settings[$this->plugin_slug]['instructions'];
+		$output .= '<br/>';
+		$output .= '<button class="bank_continue_btn" onclick="check_continue();">Continue</button>';
+		$output .= '<script type="text/javascript">';
+        $output .= 'function check_continue(){
+					window.location.href="'.$return_path.'"
+					};
+                     </script>';
+		return $output;
+	}
+}
+?>
