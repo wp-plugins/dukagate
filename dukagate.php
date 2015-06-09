0fb2db49ffc98aeb0875e9e82aaa3c0232453bda
rixeo <rixeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-09 09:06:43 +0000

    Tagging version 3.7.5.2
    git-svn-id: https://plugins.svn.wordpress.org/dukagate/trunk@1177305 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dukagate.php
+++ dukagate.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: DukaGate Shopping Cart
 Description: DukaGate Shopping Cart
-Version: 3.7.5.1
+Version: 3.7.5.2
 Author: rixeo
 Author URI: http://www.shumipress.com/
 Plugin URI: http://dukagate.info/
